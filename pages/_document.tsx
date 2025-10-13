f0a18e5ba045c7f2ee783a429f34c2e0ce7caac8
Kai-Abrorbek <abror0dev@gmail.com>
2025-10-14 07:52:12 +0900

    feat: meta tags integration on document private file

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -4,7 +4,26 @@ export default function Document() {
   console.log("DOCUMENT COMPONENT -> PAGES ROUTER");
   return (
     <Html lang="en">
-      <Head />
+      <Head>
+        <meta charSet="UTF-8" />
+        <meta name="title" content="Nestar Demo" />
+        <meta name="robots" content="index,follow" />
+        <link rel="icon" type="image/png" href="/img/logo/favicon.svg" />
+
+        {/* SEO(search engine optimizetion) */}
+        <meta
+          name="keyword"
+          content={"nestart, nestart.uz, devex mern, mern nestjs fullstack"}
+        />
+        <meta
+          name="description"
+          content={
+            "Buy and sell properties anywhere anytime in South Korea. |" +
+            "한국에서 언제 어디서나 부동산을 사고팔 수 있습니다." +
+            "Покупайте и продавайте недвижимость в любом месте и в любое время в Южной Корее."
+          }
+        />
+      </Head>
       <body>
         <Main />
         <NextScript />
