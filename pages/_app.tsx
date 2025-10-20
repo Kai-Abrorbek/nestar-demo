a801b0892c6b5a93de144f326017e7d85adbc9c3
Kai-Abrorbek <abror0dev@gmail.com>
2025-10-21 07:57:21 +0900

    feat:develop apollo client connaction

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -4,6 +4,8 @@ import { ThemeProvider, createTheme } from "@mui/material/styles";
 import { CssBaseline } from "@mui/material";
 import React, { useState } from "react";
 import { light } from "../scss/MaterialTheme";
+import { ApolloProvider } from "@apollo/client";
+import client from "@/apollo/client";
 import "../scss/app.scss";
 import "../scss/pc/main.scss";
 import "../scss/mobile/main.scss";
@@ -13,10 +15,12 @@ function MyApp({ Component, pageProps }: AppProps) {
   const [theme, setTheme] = useState(createTheme(light));
 
   return (
-    <ThemeProvider theme={theme}>
-      <CssBaseline />
-      <Component {...pageProps} />
-    </ThemeProvider>
+    <ApolloProvider client={client}>
+      <ThemeProvider theme={theme}>
+        <CssBaseline />
+        <Component {...pageProps} />
+      </ThemeProvider>
+    </ApolloProvider>
   );
 }
 
